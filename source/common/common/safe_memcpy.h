e16bea013fa9d3218f94a5b7579176315d5e7eba
grial1 <grial@google.com>
2020-12-03 12:01:17 +0000

    format fixed
    Signed-off-by: grial1 <grial@google.com>

--- source/common/common/safe_memcpy.h
+++ source/common/common/safe_memcpy.h
@@ -16,20 +16,18 @@ template <typename T1, typename T2> inline void safeMemcpy(T1* dst, T2* src) {
  * @brief Copies src to dst based on the size of dst, which must be specified.
  */
 #define safeMemcpySrc(dst, src, size)                                                              \
-do {                                                                                               \
+  do {                                                                                             \
     static_assert(sizeof(*(dst)) == size);                                                         \
     memcpy(dst, src, size);                                                                        \
-  }                                                                                                \
-  while (0)
+  } while (0)
 
 /**
  * @brief Copies src to dst based on the size of src, which must be specified.
  */
 #define safeMemcpyDst(dst, src, size)                                                              \
-do {                                                                                               \
+  do {                                                                                             \
     static_assert(sizeof(*(src)) == size);                                                         \
     memcpy(dst, src, size);                                                                        \
-  }                                                                                                \
-  while (0)
+  } while (0)
 
 } // namespace Envoy
